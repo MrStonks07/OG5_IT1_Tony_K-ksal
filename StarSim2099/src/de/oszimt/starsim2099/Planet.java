67b59fbc53289130efa0fe91897b6771fcbc951e
Tony <tony.koeksal@gmail.com>
2026-09-24 09:16:55 +0200

    Added Attributes and Methods for class "Planet"

--- StarSim2099/src/de/oszimt/starsim2099/Planet.java
+++ StarSim2099/src/de/oszimt/starsim2099/Planet.java
@@ -9,14 +9,49 @@ package de.oszimt.starsim2099;
 public class Planet {
 
 	// Attribute
+	private int AnzahlHafen;
+	private String name;
+	private double posX;
+	private double posY;
 	
 	// Methoden
+
+public String getName() {
+	return name;
+}
+
+public int getAnzahlHafen() {
+	return AnzahlHafen;
+}
+
+public void setAnzahlHafen(int anzahlHafen) {
+	AnzahlHafen = anzahlHafen;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public double getPosX() {
+	return posX;
+}
+
+public void setPosX(double posX) {
+	this.posX = posX;
+}
+
+public double getPosY() {
+	return posY;
+}
+
+public void setPosY(double posY) {
+	this.posY = posY;
+}
 
 	// Darstellung
 	public static char[][] getDarstellung() {
 		char[][] planetShape = { { '\0', '/', '*', '*', '\\', '\0' }, { '|', '*', '*', '*', '*', '|' },
 				{ '\0', '\\', '*', '*', '/', '\0' } };
 		return planetShape;
-
 	}
 }
